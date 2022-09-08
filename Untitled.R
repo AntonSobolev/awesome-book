a0ec7810cfa3b181da485744650b087a013a3c2b
AntonSobolev <ant.sobolev@gmail.com>
2022-09-07 22:08:34 -0500

    up
    up

---
+++ Untitled.R
@@ -0,0 +1,7 @@
+
+
+usethis::use_git_config(user.name = "AntonSobolev", user.email = "ant.sobolev@gmail.com")
+credentials::set_github_pat("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")
+# git remote add origin https://github.com/AntonSobolev/awesome-book
+# git push --set-upstream origin master
+gitcreds::gitcreds_set()
